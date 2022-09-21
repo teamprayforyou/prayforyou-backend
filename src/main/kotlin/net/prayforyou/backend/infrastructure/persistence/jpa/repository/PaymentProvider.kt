c661193cef36292767c9c24e6f3c1cf51aceadba
sawooook <lna1003@naver.com>
2022-09-21 19:40:57 +0900

    뻘짓 삭제 ㅋ

--- src/main/kotlin/net/prayforyou/backend/infrastructure/persistence/jpa/repository/PaymentProvider.kt
+++
@@ -1,6 +0,0 @@
-package net.prayforyou.backend.infrastructure.persistence.jpa.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PaymentProvider : JpaRepository<Payment, Long> {
-}
