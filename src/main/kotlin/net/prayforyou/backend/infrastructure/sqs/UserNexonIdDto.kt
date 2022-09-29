1d199c86a6c84f04b4274cb0f3f20a80e2f9c047
youngsuk-kim <thayer2392@gmail.com>
2022-09-29 17:32:04 +0900

    그레들, application.yml 수정

--- src/main/kotlin/net/prayforyou/backend/infrastructure/sqs/UserNexonIdDto.kt
+++
@@ -1,8 +0,0 @@
-package net.prayforyou.backend.infrastructure.sqs
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-class UserNexonIdDto {
-    val userNexonId: String? = null
-}
