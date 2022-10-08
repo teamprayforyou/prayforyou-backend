94a6688d35eaa22f75237077e1df7e3300ca23c7
youngsuk-kim <thayer2392@gmail.com>
2022-10-08 18:19:22 +0900

    전체 수정

--- src/main/kotlin/net/prayforyou/backend/application/event/EventService.kt
+++ src/main/kotlin/net/prayforyou/backend/application/event/EventService.kt
@@ -42,7 +42,7 @@ class EventService(
         val y: Double
     )
 
-    @Scheduled(fixedDelay= 1200000)
+    @Scheduled(fixedDelay= 900000)
     fun process() {
         val findTodoEvents = eventProvider.findTodoEvents()
         val findTodoUserJson = userJsonProvider.findTodoEvents()
