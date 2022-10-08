4f5968bbf94aaa25c14421b3cf2dd479d04b6b4f
youngsuk-kim <thayer2392@gmail.com>
2022-10-09 07:04:51 +0900

    전체 수정

--- src/main/kotlin/net/prayforyou/backend/infrastructure/persistence/jpa/repository/clan/ClanMatchRepository.kt
+++ src/main/kotlin/net/prayforyou/backend/infrastructure/persistence/jpa/repository/clan/ClanMatchRepository.kt
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param
 interface ClanMatchRepository: JpaRepository<ClanMatch, Long> {
     fun findByMatchId(matchId: Long): ClanMatch
 
-    @Query("select c.clanMatch from MatchUser c where c.user.userNexonId =:userNexonId")
+    @Query("select c.clanMatch from MatchUser c where c.user.userNexonId =:userNexonId order by c.clanMatch.matchStartTime desc")
     fun findClanMatchByUserNexonIdPageable(@Param(value = "userNexonId") userNexonId: Int, pageable: Pageable): Page<ClanMatch>
 
     @Query("select distinct c.clanMatch from MatchUser c where c.playClan.clanId =:clanId order by c.clanMatch.matchStartTime desc")
