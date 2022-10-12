090b8bbda2c5c0d1710fb58a2ae14d3b22c65ecc
sawook <lna1003@naver.com>
2022-10-13 00:11:04 +0900

    feat: UserSignUpRequest 엔티티 추가
    UserSignUpRequest 엔티티 추가

---
+++ src/main/kotlin/net/prayforyou/backend/domain/user/UserSignUpRequestEntity.kt
@@ -0,0 +1,48 @@
+package net.prayforyou.backend.domain.user
+
+import net.prayforyou.backend.domain.user.enums.UserSignUpStatus
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "user_sign_up_request")
+@EntityListeners(AuditingEntityListener::class)
+class UserSignUpRequestEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Column(name = "email")
+    val email: String,
+
+    @Column(name = "password")
+    val password: String,
+
+    @Column(name = "clan_unique_number")
+    val clanId: Long,
+
+    @Column(name = "nickname")
+    val nickname: String,
+
+    @Column(name = "type")
+    val type: UserSignUpStatus = UserSignUpStatus.WAIT,
+
+    @Column(name = "created_at")
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+) {
+    companion object {
+        fun from(clanId: Long, email: String, nickname: String, password: String): UserSignUpRequestEntity =
+            UserSignUpRequestEntity(
+                email = email,
+                nickname = nickname,
+                password = password,
+                clanId = clanId
+            )
+    }
+}
