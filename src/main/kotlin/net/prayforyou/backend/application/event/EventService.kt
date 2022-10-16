01b32dd8fac5ee7a606097132f893f4579cff104
bread <bread.s@kakao.com>
2022-10-16 16:24:27 +0900

    에러 수정

--- src/main/kotlin/net/prayforyou/backend/application/event/EventService.kt
+++ src/main/kotlin/net/prayforyou/backend/application/event/EventService.kt
@@ -46,7 +46,6 @@ class EventService(
         val y: Double
     )
 
-    @Scheduled(fixedDelay = 200000)
     fun process() {
         var findTodoEvents = eventProvider.findTodoEvents()
         var findTodoUserJson = userJsonProvider.findTodoEvents()
