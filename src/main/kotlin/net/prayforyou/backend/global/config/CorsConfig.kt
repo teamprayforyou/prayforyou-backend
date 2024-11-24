f5e43e99dd38fcde5a21ba57e0d96e7ef285caf4
youngsuk <youngsuk-kim@github.com>
2024-11-25 02:29:52 +0900

    cors

--- src/main/kotlin/net/prayforyou/backend/global/config/CorsConfig.kt
+++ src/main/kotlin/net/prayforyou/backend/global/config/CorsConfig.kt
@@ -9,8 +9,9 @@ class CorsConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
-            .allowedHeaders("*")
+            .allowedOrigins("*")  // 모든 도메인 허용
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드 설정
+            .allowedHeaders("*")  // 모든 헤더 허용
+            .allowCredentials(true)  // 인증 정보를 포함한 요청도 허용
     }
 }
